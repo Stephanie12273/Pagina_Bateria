ba39db4b46527c20626d3b1fc94d461e7ac671fd
Lory Stephanie Achury <est.lory.achury@unimilitar.edu.co>
2024-03-01 18:53:08 -0500

    correccion 1.1

--- js/registro.js
+++ js/registro.js
@@ -68,7 +68,8 @@ login.addEventListener("click", function (event) {
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    alert("correcto")
+    alert("Bienvenido"+ email)
+    window.location.href="Dashboard.html";
     // ...
   })
   .catch((error) => {
