26bf34f5d7d85d32f1539e7d4e609ef8bf54dc22
Camilo Garcia <yashircamilo@gmail.com>
2024-03-21 22:41:01 -0500

    Ya casi

--- js/Graficar.js
+++ js/Graficar.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getDatabase, ref, onValue, update} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-import {Chart} from "https://cdn.jsdelivr.net/npm/chart.js";
+import {chart} from "node_modules\chart.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBgBuZFXHhpoQKHyPDIObZjHVEl1R7OhgE",
