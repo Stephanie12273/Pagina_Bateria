668725443ea0c9205fb3c2690ae41f1e6e5bc190
Camilo Garcia <yashircamilo@gmail.com>
2024-03-21 20:06:56 -0500

    correciones

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -2,6 +2,7 @@ from firebase import firebase
 import pandas as pd
 import time
 
+tiempo = time.time()
 firebase = firebase.FirebaseApplication("https://proyecto-48aa4-default-rtdb.firebaseio.com/", None)
 
 df = pd.read_csv(r"C:\Users\Camilo Garcia\OneDrive - unimilitar.edu.co\Desktop\Universidad\Mantenimiento Preventido en la nube\Pagina_Bateria\Backend\TripA01.csv", encoding='latin1', sep=';')
@@ -13,6 +14,7 @@ Elevacion = df["Elevation [m]"].tolist()
 # Enviar datos a Firebase cada 5 segundos
 for i in range(0, len(velocidades), 2):
     data = {
+        'timestamp': tiempo,
         'velocidad': velocidades[i],
         'SoC': EstadoCarga[i],
         'Corriente': Corriente[i],
